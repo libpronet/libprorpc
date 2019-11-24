a5fb0daadcb7f94c3d97c363aacfac3b6e62be39
Eric Tung <libpronet@gmail.com>
2019-11-24 17:01:38 +0800

    Add supports for Visual Studio 2010/2013

---
+++ pub/lib-d/_get-win-vs2010.bat
@@ -0,0 +1,18 @@
+@echo off
+set THIS_DIR=%~sdp0
+
+copy /y %THIS_DIR%..\..\build\windows-vs2010\_debug\pro_rpc.dll            %THIS_DIR%windows-vs2010\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2010\_debug\pro_rpc.lib            %THIS_DIR%windows-vs2010\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2010\_debug\pro_rpc.map            %THIS_DIR%windows-vs2010\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2010\_debug\pro_rpc.pdb            %THIS_DIR%windows-vs2010\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2010\_debug\test_rpc_client.exe    %THIS_DIR%windows-vs2010\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2010\_debug\test_rpc_server.exe    %THIS_DIR%windows-vs2010\x86\
+
+copy /y %THIS_DIR%..\..\build\windows-vs2010\_debug64x\pro_rpc.dll         %THIS_DIR%windows-vs2010\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2010\_debug64x\pro_rpc.lib         %THIS_DIR%windows-vs2010\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2010\_debug64x\pro_rpc.map         %THIS_DIR%windows-vs2010\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2010\_debug64x\pro_rpc.pdb         %THIS_DIR%windows-vs2010\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2010\_debug64x\test_rpc_client.exe %THIS_DIR%windows-vs2010\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2010\_debug64x\test_rpc_server.exe %THIS_DIR%windows-vs2010\x86_64\
+
+pause
