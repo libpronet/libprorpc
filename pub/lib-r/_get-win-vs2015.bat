c2ece89c097367121a17d8a5533a11cfefd5ab56
Eric Tung <libpronet@gmail.com>
2021-01-03 12:31:46 +0800

    Upgrade the build tool to VS2015 on Windows

---
+++ pub/lib-r/_get-win-vs2015.bat
@@ -0,0 +1,18 @@
+@echo off
+set THIS_DIR=%~sdp0
+
+copy /y %THIS_DIR%..\..\build\windows-vs2015\_release\pro_rpc.dll            %THIS_DIR%windows-vs2015\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2015\_release\pro_rpc.lib            %THIS_DIR%windows-vs2015\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2015\_release\pro_rpc.map            %THIS_DIR%windows-vs2015\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2015\_release\pro_rpc.pdb            %THIS_DIR%windows-vs2015\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2015\_release\test_rpc_client.exe    %THIS_DIR%windows-vs2015\x86\
+copy /y %THIS_DIR%..\..\build\windows-vs2015\_release\test_rpc_server.exe    %THIS_DIR%windows-vs2015\x86\
+
+copy /y %THIS_DIR%..\..\build\windows-vs2015\_release64x\pro_rpc.dll         %THIS_DIR%windows-vs2015\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2015\_release64x\pro_rpc.lib         %THIS_DIR%windows-vs2015\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2015\_release64x\pro_rpc.map         %THIS_DIR%windows-vs2015\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2015\_release64x\pro_rpc.pdb         %THIS_DIR%windows-vs2015\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2015\_release64x\test_rpc_client.exe %THIS_DIR%windows-vs2015\x86_64\
+copy /y %THIS_DIR%..\..\build\windows-vs2015\_release64x\test_rpc_server.exe %THIS_DIR%windows-vs2015\x86_64\
+
+pause
